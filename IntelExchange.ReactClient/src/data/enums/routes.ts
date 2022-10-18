da3e89f8bb8eb72f086a8686ec1986781a6fdde2
Kontores <ethereal_alpha@hotmail.com>
2022-10-18 13:13:24 +0200

    routes fix

--- IntelExchange.ReactClient/src/data/enums/routes.ts
+++ IntelExchange.ReactClient/src/data/enums/routes.ts
@@ -2,6 +2,6 @@ export enum RoutesEnum {
     home = "/",
     dashboard = "/dashboard",
     login = "/account/login",
-    logout = "/account/logout",,
+    logout = "/account/logout",
     signup = "/account/signup"
 }
