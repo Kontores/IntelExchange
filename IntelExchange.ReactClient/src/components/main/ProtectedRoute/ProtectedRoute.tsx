0446a042232672aa73dff0e7e8a5981dd15d1b2a
Kontores <ethereal_alpha@hotmail.com>
2022-10-19 17:51:48 +0200

    protected route component created

---
+++ IntelExchange.ReactClient/src/components/main/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import { RoutesEnum } from '../../../data/enums/routes';
+import { UserRole } from '../../../data/enums/user-role';
+import { ApplicationState } from '../../../store/ApplicationState';
+import { UserState } from '../../../store/UserState';
+import { connect } from 'react-redux';
+
+type StateProps = {
+    user: UserState;
+}
+
+type ProtectedRouteProps = StateProps & {
+    path: RoutesEnum;
+    roles: UserRole[];
+    element: JSX.Element;
+};
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ path, roles, user, element }) => {
+    const isUserAuthorized = (userRoles: UserRole[], requiredRoles: UserRole[]) => (
+        requiredRoles.reduce((res: boolean, role: UserRole) => userRoles.includes(role), false));
+
+    if (!user.login) {
+        return (<Navigate to={RoutesEnum.login} />)
+    } else {
+        return (user.roles && isUserAuthorized(user.roles, roles))
+            ? (<Route path={path} element={element} />) : (<Navigate to={RoutesEnum.home} />);
+    }
+    
+}
+
+const mapStateToProps = (state: ApplicationState): StateProps => ({
+    user: state.user
+});
+
+export default connect(mapStateToProps)(ProtectedRoute);
