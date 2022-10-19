ae2f549496cd33819ca4ef11014018056bf67833
Kontores <ethereal_alpha@hotmail.com>
2022-10-19 17:50:37 +0200

    api service configured to redirect on specific responses

--- IntelExchange.ReactClient/src/services/api-service.ts
+++ IntelExchange.ReactClient/src/services/api-service.ts
@@ -1,5 +1,26 @@
 import axios from 'axios';
+import { RoutesEnum } from '../data/enums/routes';
+import { history } from '../store/configureStore';
 
-export default axios.create({
+const apiService = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 });
+
+apiService.interceptors.response.use((response) => { return response }, (error) => {
+    if (axios.isAxiosError(error)) {
+        if(error.response?.status === 401 || error.response?.status === 403) {
+            history.replace(RoutesEnum.login);
+        }
+        if (error.response?.status === 404) {
+            history.replace(RoutesEnum.home);
+        }
+    }
+
+    return Promise.reject(error);
+});
+
+
+export default apiService;
+
+
+
